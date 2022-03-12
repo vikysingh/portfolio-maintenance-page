db1711924c985236ad8fff7e36bd98e9b8fe2d44
Vikram Singh <singhvik071@gmail.com>
2022-03-12 18:46:39 +0100

    server and html setup

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const app = express()
+const PORT = 3000
+
+const path = require("path")
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.listen(PORT, () => console.log("listening on PORT: ", PORT))
